31edbb6ba18901488a81d9ff783826d4abbf2aef
Niko Cremer <n.ko@posteo.de>
2024-02-14 10:35:33 +0100

    setting up the model and the mongoose get

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,8 @@
-import { places } from "../../../lib/db";
-
-export default function handler(request, response) {
+import dbConnect from "@/db/connect.js";
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const places = await Place.find();
+  }
   return response.status(200).json(places);
 }
